e712447ceb74731be5f437ffac6d8f9027571e28
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-28 11:37:19 +0530

    InstaDSA

--- CountNumOfSubStrings.java
+++ CountNumOfSubStrings.java
@@ -1,5 +1,26 @@
 public class CountNumOfSubStrings {
+    public static long subStrCount(String s, int k) {
+        return atmost(s, k) - atmost(s, k-1);
+    }
+    public static long atmost(String s, int k) {
+        int arr[] = new int[26];
+        int dist = 0, left = 0;
+        int res = 0;
+        for(int i = 0; i<s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+            if(arr[s.charAt(i) - 'a'] == 1) dist++;
+            while(dist > k) {
+                arr[s.charAt(left) - 'a']--;
+                if(arr[s.charAt(left) - 'a'] == 0) dist--;
+                left++;
+            }
+            res = res + (i - left+1);
+        }
+        return res;
+    }
     public static void main(String[] args) {
-        
+        String s = "aba";
+        int k = 2;
+        System.out.println(subStrCount(s, k));
     }
 }
--- SortCharactersByFreq.java
+++ SortCharactersByFreq.java
@@ -1,23 +1,25 @@
 import java.util.*;
-public class SortCharactersByFreq{
+
+public class SortCharactersByFreq {
     public static String frequencySort(String s) {
-        Map<Character, Integer> map = new HashMap<>(); 
+        Map<Character, Integer> map = new HashMap<>();
         PriorityQueue<Character> pq = new PriorityQueue<>((x, y) -> map.get(y) - map.get(x));
-        for(char c : s.toCharArray()) {
+        for (char c : s.toCharArray()) {
             map.put(c, (map.getOrDefault(c, 0)) + 1);
         }
-        for(char c : map.keySet()) {
+        for (char c : map.keySet()) {
             pq.offer(c);
         }
         StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
             char ch = pq.poll();
-            for(int i = 0; i<map.get(ch); i++) {
+            for (int i = 0; i < map.get(ch); i++) {
                 sb.append(ch);
             }
         }
         return sb.toString();
     }
+
     public static void main(String[] args) {
         String s = "tree";
         System.out.println(frequencySort(s));
